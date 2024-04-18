3381ef57ab12f324aa1a9884b6d4f4ad41d650d2
Easton683 <127262863+Easton683@users.noreply.github.com>
2024-04-18 00:33:08 -0600

    AdminPatchDHCP

--- rockyAdminServerSetup.sh
+++ rockyAdminServerSetup.sh
@@ -7,14 +7,20 @@ sudo dnf update && sudo dnf upgrade -y
 sudo dnf install -y bind bind-utils
 # Configure /etc/named.conf and zone files
 
-# Install and configure DHCP server
-sudo dnf install -y dhcp
+# Install DHCP server
+sudo dnf install dhcp-server -y
 # Configure /etc/dhcp/dhcpd.conf
+sudo cd /etc/dhcp
+printf 'option domain-name "easton-jackson.com";\nsubnet 10.0.5.1 netmask 255.255.255.0 {\nrange 10.0.5.1 10.0.5.254;\noption routers 10.0.5.1;\noption domain-name-servers 8.8.8.8, 8.8.4.4;\noption subnet-mask 255.255.255.0;\noption broadcast-address 192.168.1.255;\ndefault-lease-time 600;\nmax-lease-time 7200;\n}' | sudo tee dhcp.conf
+sudo systemctl start dhcpd
+sudo systemctl enable dhcpd
 
-# Install and configure LDAP server
+# Install openLDAP server
+cd /home/eastonjackson1
 sudo dnf install curl -y
 sudo dnf install tar -y
 sudo dnf install gcc -y
+sudo dnf install sed -y
 curl -O https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.5.17.tgz
 gunzip -c openldap-2.5.17.tgz | tar xvfB -
 cd openldap-2.5.17
@@ -23,6 +29,12 @@ make depend
 make
 make test
 su root -c 'make install'
+
+# Configuring LDAP
+cd /servers/slapd
+sudo sed -i 's/olcSuffix: dc=my-domain,dc=com/olcSuffix: dc=easton-jackson,dc=com/g' slapd.ldif
+sudo sed -i 's/cn=Manager,dc=my-domain/cn=Manager,dc=easton-jackson/g' slapd.ldif
+sudo sed -i 's/olcRootPW: secret/olcRootPW: 1qaz!QAZ1qaz/g' slapd.ldif
 
 # Install and configure firewalld
 sudo dnf install -y firewalld
